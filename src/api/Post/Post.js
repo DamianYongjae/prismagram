340d49f1303ac4b5a54a69d8548c8ea02472cfdb
DamianYongjae <klick214@gmail.com>
2019-12-03 22:23:28 -0500

    kill fragments

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -2,9 +2,10 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Post: {
-    files: parent => prisma.post({ id: parent.id }).files(),
-    comments: parent => prisma.post({ id: parent.id }).comments(),
-    user: parent => prisma.post({ id: parent.id }).user(),
+    files: ({ id }) => prisma.post({ id }).files(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
+    likes: ({ id }) => prisma.post({ id }).likes(),
     isLiked: (parent, _, { request }) => {
       const { user } = request;
       const { id } = parent;
