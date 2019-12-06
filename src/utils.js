cb12a3ff97a2fd0f491040147568ef203a4c18b2
DamianYongjae <klick214@gmail.com>
2019-12-05 21:23:37 -0500

    modify sendSecretMail

--- src/utils.js
+++ src/utils.js
@@ -3,32 +3,32 @@ import nodemailer from "nodemailer";
 import sgTransport from "nodemailer-sendgrid-transport";
 import jwt from "jsonwebtoken";
 
-
 export const generateSecret = () => {
-    const randomNumber = Math.floor(Math.random()*adjectives.length);
-    return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
 
-export const sendMail = (email) => {
-    const options = {
-        auth: {         //should be .env file
-            api_user: process.env.SENDGRID_USERNAME,          
-            api_key: process.env.SENDGRID_PASSWORD
-        }
-    };
-    const client = nodemailer.createTransport(sgTransport(options));
+export const sendMail = email => {
+  const options = {
+    auth: {
+      //should be .env file
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
 
-    return client.sendMail(email);
+  return client.sendMail(email);
 };
 
 export const sendSecretMail = (address, secret) => {
-    const email = {
-        from: "nico@prismagram.com",
-        to: address,
-        subject: "Login Secret for Prismagram🔒",
-        html: `Hello! Your login secret is <strong>${secret}</strong>.<br/>Copy paste on the app/website to login`
-    }
-    return sendMail(email);
-}
+  const email = {
+    from: "prismaster@prismagram.com",
+    to: address,
+    subject: "Login Secret for Prismagram🔒",
+    html: `Hello! Your login secret is <strong>${secret}</strong><br/>Copy paste on the app/website to login`
+  };
+  return sendMail(email);
+};
 
-export const generateToken = (id) => jwt.sign({id}, process.env.JWT_SECRET);
+export const generateToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
