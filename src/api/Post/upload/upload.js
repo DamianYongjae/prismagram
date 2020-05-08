afc5920db7609cde46038b0c4ed736ce3986ac9e
DamianYongjae <klick214@gmail.com>
2020-05-08 17:21:56 -0400

    multer-s3

--- src/api/Post/upload/upload.js
+++ src/api/Post/upload/upload.js
@@ -3,9 +3,10 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     upload: async (_, args, { request, isAthenticated }) => {
-      const { caption, files } = args;
+      const { caption, files, location } = args;
       const { user } = request;
       const post = await prisma.createPost({
+        location,
         caption,
         user: { connect: { id: user.id } }
       });
--- src/upload.js
+++ src/upload.js
@@ -1,13 +1,33 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-export const uploadMiddleware = multer({
-  dest: "uploads/",
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET,
+  region: "us-east-2"
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: "prismagramdamian",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    }
+  }),
+
   limits: { fieldSize: 25 * 1024 * 1024 }
-}).single("file");
+});
+
+export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
   const {
-    file: { path }
+    file: { location }
   } = req;
-  res.json({ path });
+  res.json({ location });
 };
