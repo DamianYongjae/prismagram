60c95143244f4018d7dc9395b71090dc9af71765
DamianYongjae <klick214@gmail.com>
2020-05-13 12:53:23 -0400

    room changed

--- src/api/Room/Room.js
+++ src/api/Room/Room.js
@@ -3,6 +3,6 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Room: {
     participants: ({ id }) => prisma.room({ id }).participants(),
-    message: ({ id }) => prisma.room({ id }).messages()
+    messages: ({ id }) => prisma.room({ id }).messages()
   }
 };
